f7286ccf26a40d54f63b31183105e1facc4f9354
Jonathon <jwbialas@gmail.com>
2022-08-30 10:57:06 -0500

    commit tree a39f8417016debe01089cc3a65dd8599756d5362

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,40 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    lists = []
+    attendees.each do |attend|
+      lists.append("Hello, my name is #{attend}.")
+    end
+    lists
+end
+
+def assign_rooms(assignments)
+  l = 1
+  list = []
+  assignments.each do |person|
+    list.append("Hello, #{person}! You'll be assigned to room #{l}!")
+    l += 1
+  end
+  list
+end
+
+def printer(attendees)
+    lists1 = []
+    l = 1
+    list2 = []
+    
+    attendees.each do |attend|
+        puts "Hello, my name is #{attend}."
+    end
+    attendees.each do |person|
+        puts "Hello, #{person}! You'll be assigned to room #{l}!"
+        l += 1
+    end
+
+end
+# binding.pry
